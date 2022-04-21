5bb6f4fbd8cbebb8adec78c4a4cfd74858f47b57
KingsOg <nft33@protonmail.com>
2022-04-21 19:27:55 +0100

    Ternary operators exercise

--- main.js
+++ main.js
@@ -85,3 +85,38 @@ console.log(defaultName); //prints Stranger
 let userName = "Chike";
 defaultName = userName || "Stranger";
 console.log(defaultName); //Prints Chike
+
+// Ternary Operator
+
+let isLocked = false;
+
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
+
+//using Ternary
+isLocked ? console.log("You will need a key to open the door.") : console.log("You will not need a key to open the door.");
+
+let isCorrect = true;
+
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+//using ternary
+isCorrect ? console.log("Correct!") : console.log ("Incorrect!");
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+};
+
+//using ternary
+favoritePhrase === "Love That!" ? console.log("I love that!") : console.log("I don't love that!");
